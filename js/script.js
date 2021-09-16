aef6f9fe588005d372f8914b1c22d49ee9226ce2
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-09-16 16:10:25 +0200

    stampato e schemo messaggio + immagine

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 //Bonus:
 //Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const app = new Vue(
+    {
+        el: "#root",
+        data: {
+            message: "CIAO A TUTTI!!!!",
+            immagine: "https://c.tenor.com/Z6gmDPeM6dgAAAAM/dance-moves.gif",
+        }
+    }
+);
